f7180a21bc15b7fd827c80e6bc6a9f4ab4a792c2
Ana Julia de Souza <anaju.ofc635@gmail.com>
2025-10-07 21:11:23 -0300

    Implementa classes Smartphone, Nokia e iPhone com testes no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,43 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Nokia
+        Nokia meuNokia = new Nokia("1234-5678", "Nokia 3310", "111222333444555", 00);
+        Console.WriteLine("Teste Nokia");
+        Console.WriteLine($"Número: {meuNokia.Numero}");
+        Console.WriteLine($"Modelo: {meuNokia.Modelo}");
+        Console.WriteLine($"IMEI: {meuNokia.IMEI}");
+        Console.WriteLine($"Memória: {meuNokia.Memoria} GB");
+        meuNokia.Ligar();
+        meuNokia.ReceberLigacao();
+        meuNokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // iPhone
+        Iphone meuIphone = new Iphone("991413637-", "iPhone 14", "555444333222111", 000);
+        Console.WriteLine("Teste iPhone");
+        Console.WriteLine($"Número: {meuIphone.Numero}");
+        Console.WriteLine($"Modelo: {meuIphone.Modelo}");
+        Console.WriteLine($"IMEI: {meuIphone.IMEI}");
+        Console.WriteLine($"Memória: {meuIphone.Memoria} GB");
+        meuIphone.Ligar();
+        meuIphone.ReceberLigacao();
+        meuIphone.InstalarAplicativo("TikTok");
+
+        Console.WriteLine();
+
+        // Polimorfismo
+        Smartphone[] celulares = { meuNokia, meuIphone };
+        Console.WriteLine("Teste Polimorfismo");
+        foreach (var celular in celulares)
+        {
+            celular.Ligar();
+            celular.InstalarAplicativo("Spotify");
+        }
+    }
+}
